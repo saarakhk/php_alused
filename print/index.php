5610ca22341c60beb376bd815f00111d8e12a26b
saara.vallik <saara.vallik@khk.ee>
2018-12-13 13:06:38 +0200

    väljastamine

---
+++ print/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saara.vallik
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+
+// teksti väljastamine
+// print
+
+print('Tere maailm!<br>');
+print('PHP Alused<br>');
+print('vaata lisaks: php.net<br>');
+// echo
+echo 'käsk echo väljastab<br>';
+// väärtuste ühendamine
+
+echo 'see '.' on'.' kolmest sõnast kokku pandud<br>';
