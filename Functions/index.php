57051db5f34b4d9c84d851c6c1400ad6ab2c5117
saara.vallik <saara.vallik@khk.ee>
2018-12-17 14:59:55 +0200

    funktsiooni, mis tagastab väärtused, loomine

--- Functions/index.php
+++ Functions/index.php
@@ -23,3 +23,12 @@ function printBR($txt) {
 printBR("this is a line");
 printBR("this is a new line");
 printBR("this is yet another line");
+
+// funktsiooni, mis tagastab väärtuseid, loomine
+
+function addNums($firstnum, $secondnum) {
+    return ($firstnum + $secondnum);
+}
+print "viis pluss viis on = ".addNums(5, 5);
+
+
