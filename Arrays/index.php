1338ba32abb0784a1f1390495cdf5a0a2df76cd0
saara.vallik <saara.vallik@khk.ee>
2018-12-18 15:08:44 +0200

    andmemassiivid (tühi, lisamine jne)

--- Arrays/index.php
+++ Arrays/index.php
@@ -2,6 +2,7 @@
 
 // massivid
 
+
 $kasutajad = array(
     'alice',
     'bob',
@@ -12,4 +13,26 @@ $kasutajad = array(
 
 foreach ($kasutajad as $kasutaja) {
     echo $kasutaja.'<br>';
+}
+
+echo '<hr>';
+echo '<br>';
+
+$kasutajad1 = array();
+
+$kasutajad1[] = 'alice';
+$kasutajad1[] = 'bob';
+
+array_push($kasutajad1,'lucy');
+
+foreach ($kasutajad1 as $kasutaja1) {
+    echo $kasutaja1.'<br>';
+}
+
+echo '<hr>';
+array_shift($kasutajad1);
+array_pop($kasutajad1);
+
+foreach ($kasutajad1 as $kasutaja1) {
+    echo $kasutaja1.'<br>';
 }
