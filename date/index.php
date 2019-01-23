d8002141f3c60bb7f306fc7b718953dd4b679ff7
saarakhk <saara.vallik@khk.ee>
2019-01-23 21:32:20 +0200

    kasutajasõbraliku kuupäeva kuvamine

--- date/index.php
+++ date/index.php
@@ -9,7 +9,10 @@
 /* PHP - Ajafunktsioonid (Ülesanne 8) */
 
 /* aja loomine */
-
 echo time();
+
+/* kasutajasõbraliku kuupäeva kuvamine - kuvab hetke aja kui argumente ei lisa */
+
+echo date('d.m.Y G:i');
 
 ?>
