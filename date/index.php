eae19ff584fca2fdfe7c50e4317d198fdbfa010c
saarakhk <saara.vallik@khk.ee>
2019-01-23 22:51:17 +0200

    tehted kuupäevadega

--- date/index.php
+++ date/index.php
@@ -11,14 +11,17 @@
 /* aja loomine */
 echo time();
 
+echo '<br>';
 /* kasutajasõbraliku kuupäeva kuvamine - kuvab hetke aja kui argumente ei lisa */
 
 echo date('d.m.Y G:i');
 
+echo '<br>';
 /* ajavöönd */
 
 date_default_timezone_set('Europe/Tallinn');
 
+echo '<br>';
 /* pika kuupäeva eestistamine */
 
 // kuude massiv
@@ -30,9 +33,20 @@ $aasta = date('Y');
 //kuupeva väljastamine
 echo $paev.'.'.$kuu.' '.$aasta;
 
+echo '<br>';
 /* muu soovitud kuupäeva genereerimine */
 
 $minu_synnipaev = mktime(0, 0, 0, 11, 3, 1995);
 echo date('d.m.Y', $minu_synnipaev);
 
+echo '<br>';
+/* tehted kuupäevadega */
+
+//1min pärast hetkeaeg ja kuupäev
+echo date('d.m.Y G:i' , time()+60);
+//1h pärast hetke aeg
+echo date('d.m.Y G:i' , time()+60*60);
+// 24h pärast
+echo date('d.m.Y G:i' , time()+60*60*24);
+echo '<br>';
 ?>
