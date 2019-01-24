72336b0da1117d9d3a689e1554ba8d54ba6b6948
saara.vallik <saara.vallik@khk.ee>
2019-01-24 14:32:05 +0200

    session - loodud funktsioon vormi kuvamsieks

--- session/fnk.php
+++ session/fnk.php
@@ -1,7 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: saara.vallik
- * Date: 24.01.2019
- * Time: 14:17
- */
+
+function htmlFromFile($fileName) {
+    $fileName = 'html/'.$fileName.'.html';
+    $fp = fopen($fileName, 'r');
+    $content = fread($fp, filesize($fileName));
+    fclose($fp);
+    return $content;
+
+}
