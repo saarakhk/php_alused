6f00f3ae884b7c7086edcde9dd912e406046cbc8
saara.vallik <saara.vallik@khk.ee>
2019-01-24 15:25:17 +0200

    session - lisatud peale sisse logimist nupp millega välja logida

--- session/acts/login.php
+++ session/acts/login.php
@@ -1,8 +1,24 @@
 <?php
 
-// andmed HTML vormi poolt
+require_once '../db/conn.php';
+require_once '../db/db_fnk.php';
 
+// andmed HTML vormi poolt
 $username = $_GET['uname'];
 $password = $_GET['upass'];
 
-echo $username.' '.$password;
+// loome ühenduse andmebaasiga
+$ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+
+// küsime kasutaja andmete olemasolu andmebaasis
+$sql = 'SELECT * FROM user WHERE username = "'.$username.'"AND password = "'.md5($password).'"';
+
+$users = getData($sql, $ikt);
+
+// loome kasutaja sessiooni kui ta andmebaasis olemas
+
+if($users !== false){
+    session_start();
+    $_SESSION['user'] = $users[0];
+    header('Location: ../index.php');
+}
--- session/index.php
+++ session/index.php
@@ -1,5 +1,13 @@
 <?php
 
+session_start();
 require_once 'fnk.php';
-$loginForm = htmlFromFile('login');
-echo $loginForm;
+
+if(isset($_SESSION['user'])) {
+    echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<form action="acts/login.php" method="get"><input type="submit" value="Logi välja"></form>';
+} else {
+    $loginForm = htmlFromFile('login');
+    echo $loginForm;
+}
+
