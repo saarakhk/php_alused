16a43050513d26ba454a73be9618fb841667e05e
saara.vallik <saara.vallik@khk.ee>
2019-01-24 13:50:55 +0200

    andmebaas -

--- db/index.php
+++ db/index.php
@@ -10,5 +10,23 @@ require_once 'conn.php';
 require_once 'db_fnk.php';
 
 $iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+
+/*
 echo '<pre>';
 print_r($iktConn);
+*/
+
+// insert/update tüüpi sql tetsimine
+$sql = 'update user SET last_name = "ebatavaline" where user_id=1';
+$res = query($sql, $iktConn);
+
+// select tüüpi sql testimine
+
+$sql ='SELECT * FROM user';
+$users = getData ($sql, $iktConn);
+
+
+echo '<pre>';
+print_r($users);
+
+echo 'Tere, '.$users[0]['first_name'].' '.$users[0]['last_name'].'<br>';
