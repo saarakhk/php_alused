05d7bd28fc72fda169da936a769602453919347e
saarakhk <saara.vallik@khk.ee>
2019-01-24 19:47:18 +0200

    ül 8 - Kuva kuupäev ja kellaaeg formaadis 20.03.2013 12.31

---
+++ metshein_yl/yl8.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Saara
+ * Date: 24.01.2019
+ * Time: 19:40
+ */
+
+/* ÜLESANNE 8 */
+
+// Kuva kuupäev ja kellaaeg formaadis 20.03.2013 12.31
+
+$kp = mktime(12, 31, 0, 3, 20, 2013);
+echo date('d.m.Y', $kp);
+echo '<br>';
+
+//
+?>
