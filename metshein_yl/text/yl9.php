b64b060983fb8c532b4a87e94d2a9da6a015365a
saarakhk <saara.vallik@khk.ee>
2019-01-28 16:12:52 +0200

    tekstifunktsioonid - muuda kasutaja antud sisendit, tükelda

--- metshein_yl/text/yl9.php
+++ metshein_yl/text/yl9.php
@@ -8,3 +8,9 @@ echo 'Tere, '.ucfirst(strtolower($kasutajaNimi)).'!';
 echo '<br>';
 
 // tükelda etteantud teksti. Nt sisend->saara; väljund->s.a.a.r.a
+$sona = $_GET['sona'];
+$sona_array = (str_split($sona));
+echo strtoupper(implode('.', $sona_array));
+echo '<br>';
+
+// koosta tekstiväli, mis kuvab kasutaja sisestatud sõnumeid. Kasutaja ropud sõnad asendatakse tärnidega
