a48e62c0b3edb3439df3e0946c6a54a85f4ffc96
saarakhk <saara.vallik@khk.ee>
2019-01-29 10:46:09 +0200

    koodi taaskasutamine - lisasin menüü

--- metshein_yl/recode/index.php
+++ metshein_yl/recode/index.php
@@ -20,6 +20,12 @@
 </head>
 <body>
 <?php include('pais.php'); ?>
+<menu>
+    <a href="index.php">Avaleht</a>
+    <a href="index.php?leht=portfoolio">Portfoolio</a>
+    <a href="index.php?leht=kaart">Kaart</a>
+    <a href="index.php?leht=kontakt">Kontakt</a>
+</menu>
 <div id="sisu">
     <h2>Lorem ipsum</h2>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi, ac blandit purus hendrerit vel.</p>
