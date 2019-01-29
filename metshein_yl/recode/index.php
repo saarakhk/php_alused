2bfbef3bd909e383a8f25008df25c952433ac08a
saarakhk <saara.vallik@khk.ee>
2019-01-29 10:55:34 +0200

    koodi taaskasutamine - masiiviga koodi kontrollimine, kas selline leht on nimekirjas

--- metshein_yl/recode/index.php
+++ metshein_yl/recode/index.php
@@ -30,7 +30,9 @@
 <?php
 if(!empty($_GET['leht'])){
     $leht = htmlspecialchars($_GET['leht']);
-    if(is_file($leht.'.php')){
+    $lubatud = array('portfoolio','kaart','kontakt');
+    $kontroll = in_array($leht, $lubatud);
+    if($kontroll==true){
         include($leht.'.php');
     } else {
         echo 'Valitud lehte ei eksisteeri!';
