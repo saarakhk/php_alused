a579e1a7ea8edcbc68208c4bdc3db9e8d4a32a04
saarakhk <saara.vallik@khk.ee>
2019-01-29 10:52:22 +0200

    koodi taaskasutamine - lisasin GET funktsiooni

--- metshein_yl/recode/index.php
+++ metshein_yl/recode/index.php
@@ -26,10 +26,23 @@
     <a href="index.php?leht=kaart">Kaart</a>
     <a href="index.php?leht=kontakt">Kontakt</a>
 </menu>
+
+<?php
+if(!empty($_GET['leht'])){
+    include($_GET['leht'].'php');
+} else {
+
+
+?>
 <div id="sisu">
-    <h2>Lorem ipsum</h2>
+    <h2>Avaleht</h2>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi, ac blandit purus hendrerit vel.</p>
 </div>
+
+<?php
+    }
+?>
+
 <?php
     include('jalus.php');
     include_once('jalus.php'); // ei käivitu, kuna on juba olemas
